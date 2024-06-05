b00734111a39c3df95cb7d396252a7b8ba1464e6
Qian chen <52212525+Az-qianchen@users.noreply.github.com>
2024-06-05 11:21:52 +0800

    嵌入评论

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,15 @@
+<!--.vitepress/theme/MyLayout.vue-->
+<script setup>
+import DefaultTheme from "vitepress/theme";
+import Comments from "./vue/comments.vue";
+
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <Comments />
+    </template>
+  </Layout>
+</template>
---
+++ .vitepress/theme/vue/comments.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useData } from "vitepress";
+
+const { title } = useData();
+</script>
+
+<template>
+  <div :key="title" class="giscus">
+    <component
+      :is="'script'"
+      src="https://giscus.app/client.js"
+      data-repo="Az-qianchen/Tuclink"
+      data-repo-id="R_kgDOLdBkxQ"
+      data-category="Polls"
+      data-category-id="DIC_kwDOLdBkxc4Cdypd"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="top"
+      data-lang="zh-CN"
+      data-theme="transparent_dark"
+      data-loading="lazy"
+      crossorigin="anonymous"
+      async
+    />
+  </div>
+</template>
